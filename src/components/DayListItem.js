f0eb434829028d09f077aaff260d7568e4861f65
MaxWong03 <madmaxmmajournal@gmail.com>
2019-09-30 11:04:40 -0700

    create components, use storybook to unit test

---
+++ src/components/DayListItem.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import "components/DayListItem.scss"
+
+import classNames from "classnames/bind";
+
+export default function DayListItems(props) {
+
+  const formatSpots = (spot) => {
+    if (!spot) return "no spots remaining";
+    if (spot === 1) return "1 spot remaining";
+    else return `${spot} spots remaining`;
+  };
+
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": !props.spots
+  });
+
+  return (
+    <li 
+    className={dayClass}
+    onClick={() => props.setDay(props.name)}
+    >
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}

a0640f8ce3639351b0e2c7a446f6a68faf3d19b5
MaxWong03 <madmaxmmajournal@gmail.com>
2019-10-01 14:28:43 -0700

    create template selector file;

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,18 +1,18 @@
-  import React, { Fragment } from 'react';
+import React from 'react';
 
 import "./styles.scss";
 import Show from "./Show";
 import Empty from "./Empty";
 import Header from "./Header";
 
-export default function Appointment (props) {
+export default function Appointment(props) {
   const interview = props.interview;
   return (
     <article className="appointment">
-      <Header time={props.time}/>
-      { interview
-      ? <Show student={interview.student} interviewer={interview.interviewer}/> 
-      : <Empty />}
+      <Header time={props.time} />
+      {interview
+        ? <Show student={interview.student} interviewer={interview.interviewer} />
+        : <Empty />}
     </article>
   );
 }
